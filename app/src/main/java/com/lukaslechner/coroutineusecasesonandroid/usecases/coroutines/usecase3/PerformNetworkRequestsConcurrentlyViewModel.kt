d31c84ae937bd659d37d53ff7511eeaa8da04488
SarahElHellw <sarah.mohamed.fatah@gmail.com>
2022-05-06 21:21:11 +0200

    perform 3 network requests sequentially using the launch coroutine builder / calling 3 suspend functions  one after the other

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,13 +1,33 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                //Performing 3 network requests sequentially
+                //by calling 3 suspended functions sequentially in each call
+                //the coroutine gets suspended until the completion of the suspend function
+                //and then the coroutine resumes and executes the next suspend function and so on
+                val oreoFeatures = mockApi.getAndroidVersionFeatures(27)
+                val pieFeatures = mockApi.getAndroidVersionFeatures(28)
+                val android10Features = mockApi.getAndroidVersionFeatures(29)
+                val versionFeatures = listOf(oreoFeatures, pieFeatures, android10Features)
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network Request failed!")
+            }
+        }
 
     }
 
